9f3d9350228e40cebad19b57e4f795045cc43168
GibGyb <learn.gib@gmail.com>
2023-07-19 11:16:40 +0700

    delete comment interface

--- src/Interfaces/user.interface.ts
+++ src/Interfaces/user.interface.ts
@@ -1,37 +1,10 @@
-import { Document } from "mongoose";
-// import { QueryResult } from "../paginate/paginate";
-// import { AccessAndRefreshTokens } from "../token/token.interfaces";
+import { Document, ObjectId } from "mongoose";
 
 export interface IUser extends Document {
+  id: ObjectId;
   username: string;
   password: string;
   display_name: string;
   healthy_concern: Enumerator;
   food_allergen: string;
 }
-
-// export interface IUserDoc extends IUser, Document {
-//   isPasswordMatch(password: string): Promise<boolean>;
-// }
-
-// export interface IUserModel extends Model<IUserDoc> {
-//   isEmailTaken(
-//     email: string,
-//     excludeUserId?: mongoose.Types.ObjectId
-//   ): Promise<boolean>;
-//   paginate(
-//     filter: Record<string, any>,
-//     options: Record<string, any>
-//   ): Promise<QueryResult>;
-// }
-
-// export type UpdateUserBody = Partial<IUser>;
-
-// export type NewRegisteredUser = Omit<IUser, "role" | "isEmailVerified">;
-
-// export type NewCreatedUser = Omit<IUser, "isEmailVerified">;
-
-// export interface IUserWithTokens {
-//   user: IUserDoc;
-//   tokens: AccessAndRefreshTokens;
-// }
