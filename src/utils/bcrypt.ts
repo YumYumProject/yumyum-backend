bdf6050dbfc4073ed8d6fd054862f07da2f69a66
Eve-vasinee <134252832+Eve-vasinee@users.noreply.github.com>
2023-07-21 11:21:14 +0700

    test git

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -3,6 +3,7 @@ import bcryptjs from "bcryptjs";
 const saltRounds = 10;
 
 export const hashPassword = async (password: string): Promise<string> => {
+  console.log("yo");
   return await bcryptjs.hash(password, saltRounds);
 };
 
